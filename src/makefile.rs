7ffa414fe39e82f18744c2672f8d3f8afaef2cf4
Ivan Fraixedes <ifcdev@gmail.com>
2015-06-22 01:31:38 +0100

    Added unit test for split_targets & get_targets_from_line

--- src/makefile.rs
+++ src/makefile.rs
@@ -42,26 +42,31 @@ fn read_targets(buff: &mut io::BufRead) -> Result<Vec<String>, Error> {
     Ok(vec![])
 }
 
-fn get_targets_from_line(l: &String) -> Option<Vec<String>> {
+fn get_targets_from_line(l: &str) -> Option<Vec<&str>> {
     let parts: Vec<&str> = l.splitn(2, ':').collect();
 
     if parts.len() > 1 {
        let targets = split_targets(parts[0]);
        if targets.len() > 0 {
-           Some(targets);
+           return Some(targets);
        }
     }
 
     None
 }
 
-fn split_targets(t: &str) -> Vec<String> {
+fn split_targets(t: &str) -> Vec<&str> {
     let mut targets = vec![];
-    let re = Regex::new(r"\s?(\S+)\s?").unwrap();
+    let re = Regex::new(r"\S+").unwrap();
 
     for cap in re.captures_iter(t) {
         match cap.at(0) {
-            Some(t) => targets.push(t.to_string()),
+            Some(t) => {
+                match t.to_string().find('%') {
+                    Some(_) => continue,
+                    None => targets.push(t)
+                }
+            }
             None => continue
         }
     }
@@ -69,3 +74,54 @@ fn split_targets(t: &str) -> Vec<String> {
     targets
 }
 
+#[cfg(test)]
+mod test_target_from_line {
+    use super::get_targets_from_line;
+
+    #[test]
+    fn it_returns_targets_for_target_line() {
+        assert_eq!(get_targets_from_line("start: dev-env  "), Some(vec!["start"]));
+    }
+
+    #[test]
+    fn it_returns_empty_for_no_target_line() {
+        assert_eq!(get_targets_from_line("  $(CC) main.c"), None);
+    }
+}
+
+#[cfg(test)]
+mod test_split_targets {
+    use super::split_targets;
+
+    #[test]
+    fn it_split_one_targets() {
+        assert_eq!(split_targets("run"), vec!["run"])
+    }
+
+    #[test]
+    fn it_split_one_targets_removing_blanks() {
+        assert_eq!(split_targets("  dev "), vec!["dev"])
+    }
+
+    #[test]
+    fn it_split_two_targets() {
+        assert_eq!(split_targets("dev start"), vec!["dev", "start"])
+    }
+
+    #[test]
+    fn it_split_three_targets_with_more_than_one_space() {
+        assert_eq!(split_targets("hey   you   !"), vec!["hey", "you", "!"])
+    }
+
+    #[test]
+    fn it_consider_targets_which_are_variables() {
+        assert_eq!(split_targets("$(list)"), vec!["$(list)"])
+    }
+
+    #[test]
+    fn it_does_not_consider_targets_which_are_patterns() {
+        let empty: Vec<String> = vec![];
+
+        assert_eq!(split_targets("%.o"), empty);
+    }
+}
